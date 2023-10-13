d85ae18e3d0900a19661b3df18fcfde0ff7591ef
Salah eddine berret <146879159+SBerret3301@users.noreply.github.com>
2023-10-13 09:59:40 +0100

    Add files via upload

---
+++ the-same.py
@@ -0,0 +1,6 @@
+x = float(input("Write a number : "))
+y = float(input("Write a second one : "))
+if ( x >= 0 and y >= 0 ) or ( x <= 0 and y <= 0) :
+    print("The two numbers have the same sign")
+else :
+    print("The two numbers have a defrent sign")
